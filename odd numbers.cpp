163db1ff545da073d1ce03a5a66a47b9d33bd8c7
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-05-08 17:56:06 +0530

    To find the odd numbers with the help of for loop.

---
+++ odd numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+   int i,n;
+   for (i = 1; i <= 20; i++)
+ {
+   if (i % 2 != 0)
+   cout << i << " ";   
+}
+   cout << "\n";
+   return 0;
+
+}
+
